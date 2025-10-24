035f90ef6f4079b22cc0fc8b0f6aa17482b1b73d
Danilo Eslawan <daniloeslawanmusic@gmail.com>
2025-10-24 15:09:06 +0800

    fix: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
-import Lenis from '@studio-freight/lenis';
-import { AuthProvider } from './contexts/AuthContext';
-import Home from './pages/home';
-import Login from './pages/login';
-import ManageUnits from './pages/manage-units';
-import Profile from './pages/profile';
-import Booking from './pages/booking';
-import SignUp from './pages/signup';
-import AdminPanel from './pages/admin';
-import ManageUsers from './pages/manage-users';
-import UnitView from './pages/unit-view';
-import Updates from './pages/updates';
-import Calendar from './pages/calendar';
-import BookingDetails from './pages/booking-details';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+import { AuthProvider } from "./contexts/AuthContext";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import ManageUnits from "./pages/manage-units";
+import Profile from "./pages/profile";
+import Booking from "./pages/booking";
+import SignUp from "./pages/signup";
+import AdminPanel from "./pages/admin";
+import ManageUsers from "./pages/manage-users";
+import UnitView from "./pages/unit-view";
+import Updates from "./pages/updates";
+import Calendar from "./pages/calendar";
+import BookingDetails from "./pages/booking-details";
 
 // Global Lenis instance
 let globalLenis: Lenis | null = null;
@@ -43,26 +43,24 @@ function App() {
   }, []);
 
   return (
-    <HelmetProvider>
-      <AuthProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/unit/:id" element={<UnitView />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/manage" element={<ManageUnits />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/booking" element={<Booking />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/admin" element={<AdminPanel />} />
-            <Route path="/manageusers" element={<ManageUsers />} />
-            <Route path="/updates" element={<Updates />} />
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="/booking-details" element={<BookingDetails />} />
-          </Routes>
-        </Router>
-      </AuthProvider>
-    </HelmetProvider>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/unit/:id" element={<UnitView />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/manage" element={<ManageUnits />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/manageusers" element={<ManageUsers />} />
+          <Route path="/updates" element={<Updates />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/booking-details" element={<BookingDetails />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
