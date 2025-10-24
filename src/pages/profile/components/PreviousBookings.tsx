0f0f87cf5e42e68587656fd8ffa3a68a78ad3e8d
Jeric B. Mata <156207726+Beanlon@users.noreply.github.com>
2025-10-24 14:44:04 +0800

    feat: turned profile into components

--- src/pages/profile/components/PreviousBookings.tsx
+++ src/pages/profile/components/PreviousBookings.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+//hi
 type Props = {
   onMostRecent: () => void;
 };
--- src/pages/profile/components/ProfileHeader.tsx
+++ src/pages/profile/components/ProfileHeader.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import type { UserProfile } from '../../../types/user';
 
+
+//hi
 type Props = {
   profile: UserProfile;
   onEditAccount: () => void;
